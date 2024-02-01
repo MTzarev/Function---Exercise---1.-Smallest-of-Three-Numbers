0459320554ca6255374f271c579f8687d7818b8e
Miroslav Tzarev <mtzarev@gmail.com>
2024-02-01 13:18:57 +0200

    Add files via upload

---
+++ 1. Smallest of Three Numbers.js
@@ -0,0 +1,8 @@
+function smallestOfThreeNumbers(first, second, third) {
+
+    console.log(Math.min(first, second, third));
+}
+smallestOfThreeNumbers(2, 5, 3);
+smallestOfThreeNumbers(600, 342, 123);
+smallestOfThreeNumbers(25, 21, 4);
+smallestOfThreeNumbers(2, 2, -2);
